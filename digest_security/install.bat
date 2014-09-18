a94d751f5cf1658448904f8fda9f0bc593f24e5b
alexkhromov <hrom1981@gmail.com>
2014-09-18 16:22:13 +0300

    move project witn security authentication to digest_security/person folder

--- digest_security/install.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set currentDirectory=%cd%
-
-sqlplus / as sysdba @%currentDirectory%\userScript.sql 
-exit|sqlplus test1rest/test1rest @%currentDirectory%\dbScript.sql
-exit|sqlplus test1rest/test1rest @%currentDirectory%\insertScript.sql
-
-mvn clean install
-pause
--- digest_security/userScript.sql
+++
@@ -1,5 +0,0 @@
-DROP USER test1rest CASCADE;
-CREATE USER test1rest IDENTIFIED BY test1rest;
-grant connect, resource, dba to test1rest;
-connect test1rest/test1rest;
-exit;
